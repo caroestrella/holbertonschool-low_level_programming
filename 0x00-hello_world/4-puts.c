30b8487262b2e325fbf000e4ffab2b1e2e62338c
caroestrella <caroestre@gmail.com>
2022-02-10 17:48:16 -0500

    Add: C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-/*function main is fun*/
-
 int main(void)
 {
 char msg[] = "\"Programming is like building a multilingual puzzle";
 puts(msg);
 return (0);
 }
-
