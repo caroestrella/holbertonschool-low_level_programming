5ca6d1f89bd74656bbe71d675b34dbe7bb89f293
caroestrella <caroestre@gmail.com>
2022-02-10 18:21:57 -0500

    Add: C file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - from main function you can call other functions
+*
+* Return: always 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
