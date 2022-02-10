41f6341c7b2826a8abd1277438c9515671e5ef50
caroestrella <caroestre@gmail.com>
2022-02-10 16:53:29 -0500

    Add: script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,7 @@
-/* "Programming is like building a multilingual puzzle */
+#include <stdio.h>
+
+int main ()
+  (
+   printf ("Programming is like building a multilingual puzzle\n");
+   return (0);
+   )
