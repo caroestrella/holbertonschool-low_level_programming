ce7c21c4b8c5d9462c343f3d5633b59cba617adf
caroestrella <caroestre@gmail.com>
2022-02-15 15:34:42 -0500

    Add:Cfile

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,11 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
+for (int x = 'a'; x <= 'z'; x++)
+{
+	_putchar(x);
+}
 _putchar('\n');
 }
