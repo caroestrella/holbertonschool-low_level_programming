e3041936103b78f9c156820b2f42638890ee3e5a
caroestrella <caroestre@gmail.com>
2022-02-15 15:56:11 -0500

    Add: C files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet a to z twice
+ *
+* Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
