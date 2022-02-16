e2016b028516bc1b0b7be8db3727b300c61effd2
caroestrella <caroestre@gmail.com>
2022-02-16 01:14:32 -0500

    Add: C files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+	*	jack_bauer - prints every minute in the life of jack bauer
+	*	Return: no return just deathhhhhhhhhhhhhhh ha ha ha
+	*/
+void jack_bauer(void)
+{
+int i;
+
+for (i = '00:00' ; i < '23:59' ; i++)
+
+{
+;
+}
+}
