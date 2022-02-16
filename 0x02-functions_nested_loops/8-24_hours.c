fea5cb6e84a95b400df693e3c44e2d8fae8591cc
caroestrella <caroestre@gmail.com>
2022-02-16 01:30:41 -0500

    Add: C files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,11 +5,19 @@
 	*/
 void jack_bauer(void)
 {
-int i;
+	int hrs;
+	int mins;
 
-for (i = '00:00' ; i < '23:59' ; i++)
-
-{
-;
-}
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(58);
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
